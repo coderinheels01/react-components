ef67dd815ce36d1beeb849613b88b6fe9fb68217
emiaun <emily.aung@nasdaq.com>
2020-10-31 14:43:09 -0400

    remove ReactDom strict mode

--- src/index.js
+++ src/index.js
@@ -1,15 +1,10 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { render } from "react-dom";
 import "./index.css";
 import Docs from "./docs/Docs";
 import * as serviceWorker from "./serviceWorker";
 import "../node_modules/highlight.js/styles/ocean.css";
-ReactDOM.render(
-  <React.StrictMode>
-    <Docs />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+render(<Docs />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
