95e1d861b20dd09754da993feb6b71a1c5d9ba58
emiaun <emily.aung@nasdaq.com>
2020-10-31 12:36:28 -0400

    with default props

--- src/components/HelloWorld/HelloWorld.js
+++ src/components/HelloWorld/HelloWorld.js
@@ -2,10 +2,14 @@ import React from "react";
 import PropTypes from "prop-types";
 
 const HelloWorld = message => {
-    return <div>Hello World</div>
+    return <div>Hello {message}</div>
 }
 
 HelloWorld.propTypes = {
     message: PropTypes.string
+}
+
+HelloWorld.defaultProps = {
+    message: "World"
 }
 export default HelloWorld;
