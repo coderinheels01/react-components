0b6da9333e7b27302f3423e4199e329785ff59fb
emiaun <emily.aung@nasdaq.com>
2020-11-02 20:57:23 -0500

    add font awesome svg icon in solid and add EyeIcon component

---
+++ src/components/EyeIcon/EyeIcon.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEye } from "@fortawesome/free-solid-svg-icons";
+
+/** Eye icon */
+const EyeIcon = () => {
+  return <FontAwesomeIcon icon={faEye} />;
+};
+
+export default EyeIcon;
---
+++ src/docs/examples/EyeIcon/ExampleEyeIcon.js
@@ -0,0 +1,8 @@
+import React from "react";
+import EyeIcon from "react-components/EyeIcon/";
+
+const ExampleEyeIcon = () => {
+  return <EyeIcon />;
+};
+
+export default ExampleEyeIcon;
