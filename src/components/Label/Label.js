d69181e035afc26d2a71ac37dd3d2aba41622691
emiaun <emily.aung@nasdaq.com>
2020-11-10 09:17:22 -0500

    add faker library and unit test for Label

--- src/components/Label/Label.js
+++ src/components/Label/Label.js
@@ -2,21 +2,20 @@ import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
 
+export const LabelWrapper = styled.label`
+  display: block;
+`;
+export const LabelRequired = styled.span`
+  color: red;
+`;
+
 /** Label with required field display and  html*/
-const Label = ({ label, htmlFor, required }) => {
-  const LabelWrapper = styled.label`
-    display: block;
-  `;
-  const LabelRequired = styled.span`
-    color: red;
-  `;
-  return (
-    <LabelWrapper htmlFor={htmlFor}>
-      {label}
-      {required && <LabelRequired> * </LabelRequired>}
-    </LabelWrapper>
-  );
-};
+const Label = ({ label, htmlFor, required }) => (
+  <LabelWrapper htmlFor={htmlFor}>
+    {label}
+    {required && <LabelRequired> * </LabelRequired>}
+  </LabelWrapper>
+);
 
 Label.propTypes = {
   /** Label text */
---
+++ src/components/Label/Label.test.js
@@ -0,0 +1,51 @@
+import React from "react";
+import { shallow } from "enzyme";
+import faker from "faker";
+import Label, { LabelWrapper, LabelRequired } from "./Label";
+
+describe("Label", () => {
+  let props;
+  const shallowLabel = () => shallow(<Label {...props} />);
+
+  beforeEach(() => {
+    props = {
+      label: faker.random.word(),
+      htmlFor: faker.random.word(),
+      required: faker.random.boolean()
+    };
+  });
+  it("should render a 'LabelWrapper' ", () => {
+    expect(shallowLabel().find(LabelWrapper).length).toBe(1);
+  });
+
+  it("should pass 'htmlFor' into 'LabelWrapper' ", () => {
+    expect(
+      shallowLabel()
+        .find(LabelWrapper)
+        .props()
+    ).toMatchObject({
+      htmlFor: props.htmlFor
+    });
+  });
+
+  it("should render 'label' inside 'LabelWrapper' ", () => {
+    expect(
+      shallowLabel()
+        .find(LabelWrapper)
+        .text()
+    ).toEqual(props.label);
+  });
+
+  describe("if 'required' is 'true'", () => {
+    beforeEach(() => {
+      props.required = true;
+    });
+    it("should render a 'LabelRequired' inside 'LabelWrapper' ", () => {
+      expect(
+        shallowLabel()
+          .find(LabelWrapper)
+          .find(LabelRequired).length
+      ).toBe(1);
+    });
+  });
+});
