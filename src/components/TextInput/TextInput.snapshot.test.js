4cb3fb1aeb4931df9412875fe41055cedcc5dddf
emiaun <emily.aung@nasdaq.com>
2020-11-12 08:49:24 -0500

    snapshot test for TextInput

---
+++ src/components/TextInput/TextInput.snapshot.test.js
@@ -0,0 +1,52 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import TextInput from "./TextInput";
+
+describe("TextInput", () => {
+  let props;
+  let textInputTree;
+
+  describe("TextInput with type text", () => {
+    beforeEach(() => {
+      props = {
+        htmlId: "test html id ",
+        label: "test label",
+        required: true,
+        name: "test name",
+        type: "text",
+        placeholder: "test placeholder",
+        children: <div />,
+        onChange: jest.fn(),
+        value: "test value",
+        error: "some error"
+      };
+      textInputTree = renderer.create(<TextInput {...props} />);
+    });
+
+    it("render TextInput of type text", () => {
+      expect(textInputTree).toMatchSnapshot();
+    });
+  });
+
+  describe("TextInput with type password", () => {
+    beforeEach(() => {
+      props = {
+        htmlId: "test html id ",
+        label: "test label",
+        required: true,
+        name: "test name",
+        type: "password",
+        placeholder: "test placeholder",
+        children: <div />,
+        onChange: jest.fn(),
+        value: "test value",
+        error: "some error"
+      };
+      textInputTree = renderer.create(<TextInput {...props} />);
+    });
+
+    it("render TextInput of type password", () => {
+      expect(textInputTree).toMatchSnapshot();
+    });
+  });
+});
